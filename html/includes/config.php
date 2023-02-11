66250cd30fbc811f3db80fbc8d0afa31fbddb235
Jaisonjohn78 <jaisonjohn78.com@gmail.com>
2023-02-11 09:00:21 +0530

    Config changes

--- html/includes/config.php
+++ html/includes/config.php
@@ -5,9 +5,9 @@
 // $password = "";
 // $database = "easyearn_master_db";
 
-$hostname = "sql103.epizy.com";
-$username = "epiz_31513890";
-$password = "o8CWyaQlmJe";
+$hostname = "127.0.0.1:3306";
+$username = "u915512721_gZ2KM";
+$password = "Easyearn@1";
 $database = "u915512721_tdUyE";
 
 $conn = mysqli_connect($hostname, $username, $password, $database) or die("Database connection failed");
